2289724b53d9fb99b25ae812fe277a6d17ac820b
Cyberpatinho <edsonkohisa@gmail.com>
2021-06-25 17:48:12 -0300

    .

--- Módulo 2 - MySQL, Spring, Docker/Blog Pessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Módulo 2 - MySQL, Spring, Docker/Blog Pessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -8,8 +8,12 @@ import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +26,7 @@ public class PostagemController {
 	@Autowired
 	private PostagemRepository repository;
 	
-	@GetMapping("/all")
+	@GetMapping
 	public ResponseEntity<?> findAll(){
 		return ResponseEntity.status(200).body(repository.findAll());
 	}
@@ -32,9 +36,25 @@ public class PostagemController {
 		return ResponseEntity.status(200).body(repository.findById(id));
 	}
 	
-	@GetMapping
-	public ResponseEntity<?> findByTitulo(@RequestParam String titulo){
-		return ResponseEntity.status(200).body(repository.findByTitulo(titulo));
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<?> findByTitulo(@PathVariable String titulo){
+		return ResponseEntity.status(200).body(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
+	@PostMapping
+	public ResponseEntity<?> post(@RequestBody Postagem postagem){
+		return ResponseEntity.status(201).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> put(@RequestBody Postagem postagem){
+		return ResponseEntity.status(200).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
 	
 }
--- Módulo 2 - MySQL, Spring, Docker/Blog Pessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ Módulo 2 - MySQL, Spring, Docker/Blog Pessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
-	public List<Postagem> findByTitulo (String titulo);
-	public List<Postagem> findById (long id);
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	public Postagem findById (long id);
 
 }
