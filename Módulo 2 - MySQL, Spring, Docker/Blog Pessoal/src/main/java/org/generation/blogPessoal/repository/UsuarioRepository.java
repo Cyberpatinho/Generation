20d19181f2c793da9e84bfce1f782e3e90ad924b
Cyberpatinho <edsonkohisa@gmail.com>
2021-07-07 23:25:05 -0300

    .

--- Módulo 2 - MySQL, Spring, Docker/Blog Pessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ Módulo 2 - MySQL, Spring, Docker/Blog Pessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	public List<Usuario> findAllByDescricaoContainingIgnoreCase(String descricao);
 	
 	public Optional<Usuario> findByUsuario(String usuario);
 	
